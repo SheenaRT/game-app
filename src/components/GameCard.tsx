4b0d6f7103815e8b4d5843ff02c69b26a741107f
Sheena Thomas <sheena@missionreadyhq.com>
2025-03-06 22:23:40 +1300

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,10 +11,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
+    <Card width={200} borderRadius={10} overflow='hidden'>
       <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize='2l'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
+
+const GameCardSkeleton = () => {
+  return (
+    <Card
+      width='250px'
+      padding='20px'
+      borderRadius={10}
+      overflow='hidden'
+    >
+      <Skeleton height='150px' />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
